0d2db2004e78fd794cb14de0b4fc72629386684b
Erick Ruiz <erick.ruiz@capstechnology.co>
2022-09-11 11:11:09 -0500

    añadimos nuevos test a la clase artifical

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import unittest
 from armas import Armas
 from humano import Humano
+from alien import Alien
+from artificial import Artificial
 from superhumano import Superhumano
 from debilidades import Debilidades
 from personalidad import Personalidad
@@ -10,6 +12,9 @@ class TestNewLigue(unittest.TestCase):
 
     personaje_superhumano = Superhumano("La viuda negra", "Natasha", 1.72, 68.4, Genre.FEMALE, Morality.HERO, Identity.PUBLIC)
     personaje_humano = Humano("El joker", "Unknown", 1.78, 73.0, Genre.MALE, "Blanco", "Marrones", "Negro")
+    personaje_alien = Alien("Quick Silver", "Pietro", "1.84", "85", Genre.MALE, "Jupiter")
+    personaje_artificial = Artificial("Ultron", "Ultron", "1.90", "80", Genre.MALE, "Tony Stark" )
+    
     debilidad = Debilidades("aragnofobia", "trauma de niñez", 2.4, "arañas")
     personalidad = Personalidad("Arrogancia", "De nacimiento", Personality.EXTROVERT, Character.IMPASSIONED)
     armas = Armas("Martillo de thor", "Fue un regalo de loki", "0.90")
@@ -29,8 +34,18 @@ class TestNewLigue(unittest.TestCase):
         self.assertTrue(self.personaje_humano.add(self.personalidad))
 
     # Tests clase Alien
+    def test_add_alien(self):
+        self.assertTrue(self.personaje_alien.add(self.armas))
+
+    def test_enemigo_alien(self):
+        self.assertTrue(self.personaje_alien.enemigo(self.personaje_artificial))
 
     # Tests clase Artificial
+    def test_add_artificial(self):
+        self.assertTrue(self.personaje_artificial.add(self.personalidad))
+
+    def test_enemigo_artificial(self):
+        self.assertTrue(self.personaje_artificial.enemigo(self.personaje_alien))
 
 
 
