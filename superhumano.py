8b6ee7489e1a0cac26b35a3a24eb41f45ceadc1b
Erick Ruiz <erick.ruiz@capstechnology.co>
2022-10-14 20:20:54 -0500

    Cambiamos valor de inicializacion por defecto

--- superhumano.py
+++ superhumano.py
@@ -3,7 +3,7 @@ from enumerations import Genre
 
 class Superhumano(PersonajeBuilder):
 
-    def __init__(self, alias = "",real_name = "", height = 0.0, weight = 0.0,genre = Genre.NONE, characterizations = [], liga = "", enemigo = None) -> None:
+    def __init__(self, alias = "",real_name = "", height = 0.0, weight = 0.0,genre = Genre.NONE, characterizations = None, liga = "", enemigo = None) -> None:
         self.alias = alias
         self.real_name = real_name
         self.height = height
